c0d1c488d4fe9b797c183c3ba0000c231e01280f
reagangonzales <144473123+reagangonzales@users.noreply.github.com>
2025-12-08 14:20:03 -0500

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -5,15 +5,20 @@ from code_two import UserProfile, RecommendationEngine
 # UserProfile Class
 # 1. Create UserProfile
 profile = UserProfile(
-    preferred_genres=["FPS","Action"],
+    preferred_genres=["FPS","Action", "Simulation"],
     max_game_length=30,
     skill_level="Beginner",
     platforms=["PC","Xbox"],
     multiplayer_preference=True
 )
 # UserProfile Methods
+print("------------------------------")
+print("Display Preferences")
 profile.display_preferences()
+print("------------------------------")
+print("Update Preferences")
 profile.update_preferences(max_game_length=40)
+profile.display_preferences()
 # ------------------------------------------------
 # Game + GameLibrary
 library = GameLibrary()
@@ -23,22 +28,30 @@ library.add_game(apex_legends)
 # Other way to add games
 library.add_game(Game("God of War", ["Action"], 25, "Intermediate", ["PlayStation"], False))
 library.add_game(Game("Stardew Valley", ["Simulation"], 50, "Beginner", ["PC"], False))
+print("------------------------------")
+print("Display Game Library Method")
+library.display_info()
 ## Way one allows for this
 # Game class method
+print("------------------------------")
+print("Game Display Method")
 apex_legends.display_info()
 # Remove games - lets switch out a game
+print("------------------------------")
+print("Remove and add to Game Library")
 library.remove_game(Game("Stardew Valley", ["Simulation"], 50, "Beginner", ["PC"], False))
 library.add_game(Game("Minecraft", ["Simulation"], 60,"Beginner",["PC","Xbox","PlayStation"], True))
+library.display_info()
 # Filter games
-filtered = library.filter_games(profile)
+print("------------------------------")
+print("Filtering Game Library - perfect match ignoring game length")
+filtered = library.filter_games(profile, filter_length=False)
 # Rank games
+print("------------------------------")
+print("Ranking Game Library by my user profile")
 ranked = library.rank_games(profile)
 # -----------------------------------------------
 # Recommendation Engine
-
-
-
-
 
 
 
